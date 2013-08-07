57f21048c4e38ad2b99724b5e898d971db791afc
Denis Mosolov <denismosolov@gmail.com>
2013-08-07 16:08:48 +0400

    Rollback d7ec809bd54a5656f7125ecbe9cfd9f41d3d3b94

--- src/js/onebutton-mep-player.js
+++ src/js/onebutton-mep-player.js
@@ -417,7 +417,6 @@
 
 			if (!(mf.isAndroid && t.options.AndroidUseNativeControls) && !(mf.isiPad && t.options.iPadUseNativeControls) && !(mf.isiPhone && t.options.iPhoneUseNativeControls)) {
 				// add user-defined features/controls
-				/*
 				for (featureIndex in t.options.features) {
 					feature = t.options.features[featureIndex];
 					if (t['build' + feature]) {
@@ -431,7 +430,7 @@
 						}
 					}
 				}
-				*/
+
 				t.container.trigger('controlsready');
 
 				// reset all layers and controls
